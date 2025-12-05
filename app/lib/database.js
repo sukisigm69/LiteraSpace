66459e73eb12735d4eceec7ba57469824fb4ba8f
abigail byann <abigailbyann@gmai.com>
2025-12-06 02:25:49 +0700

    deletefie

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,9 +1,11 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'pwdloginregister',
+export const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
-
-export default connection
