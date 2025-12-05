e240bad66a2bc66c874febaa75b00712b39d03f6
abigail byann <abigailbyann@gmai.com>
2025-12-06 02:58:07 +0700

    delie

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,11 +1,9 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
-export const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true
-  }
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'pwdloginregister',
 });
+
+export default connection
